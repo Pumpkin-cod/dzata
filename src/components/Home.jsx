4fda6b32e8f9fdd8a2f7f289532df01a91baf568
Catherine Okang-Boye <katieboye44@gmail.com>
2024-01-15 21:05:37 +0000

    changes made

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,10 @@
 import React from 'react';
 import About from './About';
 import Metrics from './Metrics';
-import Blog from './blog';
-import Footer from './footer';
-import Hero from './hero';
-import Navbar from './navbar';
+import Blog from './Blog';
+import Footer from './Footer';
+import Hero from './Hero';
+import Navbar from './Navbar';
 
 const Home = () => {
 
